230bec1cc3dfa44a286ae12670de7b37631ae69b
TangJjin <fygjbrdhvjj356@gmail.com>
2026-05-23 20:26:56 +0800

    update drone_qt

--- src/drone_qt_2/src/airborne_node.cpp
+++ src/drone_qt_2/src/airborne_node.cpp
@@ -303,7 +303,7 @@ bool AirborneNode::saveMissionYamlToFile(
     std::string &error_message)
 {
     //将接收到的yaml字符串保存到文件中，并返回保存路径和错误信息
-    const std::string dir_path = "/home/orangepi/drone_ws/install/drone_control/share/drone_control/config";
+    const std::string dir_path = "/home/orangepi/drone_ws/install/drone_mission/share/drone_mission/config";
     const std::string file_path = dir_path + "/ground_mission.yaml";
 
     //使用std::filesystem创建目录，如果目录不存在的话
