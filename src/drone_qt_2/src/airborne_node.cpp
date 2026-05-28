bc7fac15202f2ca500cf4a9b92d25ce0f5b1314f
TangJjin <fygjbrdhvjj356@gmail.com>
2026-05-28 19:08:23 +0800

    改路径

--- src/drone_qt_2/src/airborne_node.cpp
+++ src/drone_qt_2/src/airborne_node.cpp
@@ -339,7 +339,7 @@ bool AirborneNode::saveMissionYamlToFile(
     std::string &error_message)
 {
     //将接收到的yaml字符串保存到文件中，并返回保存路径和错误信息
-    const std::string dir_path = "/home/bosen/drone_project/src/drone_mission/config";
+    const std::string dir_path = "/home/orangepi/drone_ws/src/drone_mission/config";
     const std::string file_path = dir_path + "/ground_mission.yaml";
 
     //使用std::filesystem创建目录，如果目录不存在的话
