f975103a109b2663315c165d3492c795cbcf5923
TheMIrddddddddd <256951042@qq.com>
2026-06-28 16:35:52 +0800

    添加D435I调参界面

--- src/drone_perception/include/drone_perception/qr_vision_node.hpp
+++ src/drone_perception/include/drone_perception/qr_vision_node.hpp
@@ -1,17 +1,23 @@
 #pragma once
 
+#include <array>
 #include <chrono>
 #include <cstdint>
+#include <cstddef>
 #include <memory>
 #include <string>
 #include <vector>
 
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/core/types.hpp>
+#include <rcl_interfaces/msg/parameter_descriptor.hpp>
+#include <rcl_interfaces/msg/set_parameters_result.hpp>
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/synchronizer.h>
 #include <opencv2/core/mat.hpp>
 #include <rclcpp/node.hpp>
+#include <rclcpp/parameter_client.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/subscription.hpp>
 #include <rclcpp/time.hpp>
@@ -71,6 +77,44 @@ private:
     int pad_y{0};
   };
 
+  struct CameraNumericControl
+  {
+    std::string parameter_name;
+    std::string label;
+    int fallback_min{0};
+    int fallback_max{100};
+    int fallback_default{0};
+    int min_value{0};
+    int max_value{100};
+    int current_value{0};
+    int last_sent_value{0};
+    int trackbar_value{0};
+    rclcpp::ParameterType parameter_type{rclcpp::ParameterType::PARAMETER_INTEGER};
+    bool enabled{true};
+    bool send_pending{false};
+  };
+
+  struct CameraBoolControl
+  {
+    std::string parameter_name;
+    std::string label;
+    bool current_value{false};
+    bool last_sent_value{false};
+    bool enabled{true};
+    bool send_pending{false};
+    cv::Rect true_rect{};
+    cv::Rect false_rect{};
+  };
+
+  struct CameraTrackbarContext
+  {
+    QrVisionNode *node{nullptr};
+    std::size_t index{0U};
+  };
+
+  static constexpr std::size_t kCameraNumericControlCount = 3U;
+  static constexpr std::size_t kCameraBoolControlCount = 3U;
+
   typedef message_filters::sync_policies::ApproximateTime<
       sensor_msgs::msg::Image,
       sensor_msgs::msg::Image>
@@ -114,6 +158,54 @@ private:
       const cv::Mat &color_image,
       const DepthSampleResult &center_depth);
 
+  void initializeCameraControls();
+
+  void initializeCameraControlDefaults();
+
+  void displayCameraControlsWindow();
+
+  void updateCameraControlClient();
+
+  void requestCameraControlDescriptors();
+
+  void requestCameraControlValues();
+
+  void applyCameraControlDescriptors(
+      const std::vector<rcl_interfaces::msg::ParameterDescriptor> &descriptors);
+
+  void applyCameraControlValues(
+      const std::vector<rclcpp::Parameter> &parameters);
+
+  void updateCameraControlEnableStates();
+
+  void sendPendingCameraControlParameters();
+
+  void handleCameraParameterSetResults(
+      const std::vector<rclcpp::Parameter> &parameters,
+      const std::vector<rcl_interfaces::msg::SetParametersResult> &results);
+
+  void handleCameraTrackbarChange(std::size_t index, int position);
+
+  void handleCameraControlsClick(int x, int y);
+
+  void setCameraBoolControl(std::size_t index, bool value);
+
+  void updateCameraTrackbarPosition(std::size_t index);
+
+  void drawCameraBoolControl(
+      cv::Mat &panel,
+      CameraBoolControl &control,
+      int y);
+
+  static void handleCameraTrackbarCallback(int position, void *userdata);
+
+  static void handleCameraControlsMouseCallback(
+      int event,
+      int x,
+      int y,
+      int flags,
+      void *userdata);
+
 #if DRONE_PERCEPTION_HAS_BPU
   void drawBpuDetections(cv::Mat &display) const;
 
@@ -147,6 +239,8 @@ private:
   std::string camera_info_topic_;
   std::string rgbd_topic_;
   std::string window_name_;
+  std::string camera_param_node_;
+  std::string camera_controls_window_name_;
 
   std::string bpu_model_path_;
   std::string ocr_rec_model_path_;
@@ -158,7 +252,16 @@ private:
   bool enable_bpu_ = false;
   bool enable_bpu_ocr_ = false;
   bool use_rgbd_ = false;
+  bool camera_controls_enabled_ = false;
   mutable bool debug_window_created_ = false;
+  bool camera_controls_window_created_ = false;
+  bool camera_controls_updating_trackbar_ = false;
+  bool camera_param_node_online_ = false;
+  bool camera_param_describe_requested_ = false;
+  bool camera_param_describe_in_flight_ = false;
+  bool camera_param_get_requested_ = false;
+  bool camera_param_get_in_flight_ = false;
+  bool camera_param_set_in_flight_ = false;
   int log_throttle_ms_ = 500;
   int sample_radius_px_ = 10;
   int shelf_code_stable_frames_ = 3;
@@ -173,6 +276,13 @@ private:
   CodeStabilityState pkg_code_state_;
 
   DepthProcessor depth_processor_;
+
+  rclcpp::AsyncParametersClient::SharedPtr camera_param_client_;
+  std::array<CameraNumericControl, kCameraNumericControlCount> camera_numeric_controls_{};
+  std::array<CameraBoolControl, kCameraBoolControlCount> camera_bool_controls_{};
+  std::array<CameraTrackbarContext, kCameraNumericControlCount> camera_trackbar_contexts_{};
+  rclcpp::Time last_camera_param_send_time_;
+  std::string camera_controls_status_text_;
 
 #if DRONE_PERCEPTION_HAS_BPU
   std::unique_ptr<BpuYoloDetector> bpu_detector_;
