22c7eafec68c17194aba559017d7d9d6f914001c
TheMIrddddddddd <256951042@qq.com>
2026-07-20 19:51:28 +0800

    更新模型识别模型

--- src/drone_perception/src/qr_vision_node.cpp
+++ src/drone_perception/src/qr_vision_node.cpp
@@ -522,7 +522,7 @@ void QrVisionNode::declareParameters()
       DRONE_PERCEPTION_HAS_BPU != 0);
   bpu_model_path_ = this->declare_parameter<std::string>(
       "bpu_model_path",
-      "/home/sunrise/warehouse_ws/src/drone_perception/new_640x640_nv12.bin");
+      "/home/sunrise/warehouse_ws/src/drone_perception/best(2)_bayese_640x640_nv12.bin");
   ocr_rec_model_path_ = this->declare_parameter<std::string>(
       "ocr_rec_model_path",
       "/home/sunrise/warehouse_ws/src/drone_perception/en_PP-OCRv3_rec_48x320_rgb.bin");
