7fa6cecffa957677dfa37acec382dd24533704ec
TangJjin <fygjbrdhvjj356@gmail.com>
2026-08-01 03:47:02 +0800

    你的提交说明

--- src/drone_warehouse/src/car_link_bridge.cpp
+++ src/drone_warehouse/src/car_link_bridge.cpp
@@ -36,7 +36,7 @@ CarLinkBridge::CarLinkBridge()
     : rclcpp::Node("ground_car_link_bridge")
 {
     serial_port_ = this->declare_parameter<std::string>(
-        "serial_port", "/dev/ttyS3");
+        "serial_port", "/dev/warehouse_car_serial");
     baud_rate_ = this->declare_parameter<int>("baud_rate", 115200);
 
     setupRosInterfaces();
